0f17f680af288f79e23d5f2f217b64c4751a3485
alfcastillo90 <cacr1990@gmail.com>
2019-12-21 19:31:47 -0300

    [ADD]: estilos, y componente de pruebas

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,31 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MiComponente></MiComponente>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from './components/MiComponente.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
+@import './assets/style.css'
 </style>
