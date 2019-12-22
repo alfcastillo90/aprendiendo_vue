78d53cebf09365359e417bd31df8f7e2ed344ade
alfcastillo90 <cacr1990@gmail.com>
2019-12-21 22:40:54 -0300

    [FIX]: formato del componentre LastArticles y nombre de clase en App.uve

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,22 @@
   <div id="app">
     <HeaderComponent></HeaderComponent>
     <Slider></Slider>
-    <div class="cender">
+    <div class="center">
       <LastArticles></LastArticles>
       <Sidebar></Sidebar>
+      <div class="clearfix"></div>
     </div>
-    <div class="clearfix"></div>
-      <FooterComponent></FooterComponent>
+    <FooterComponent></FooterComponent>
   </div>
 </template>
-
 <script>
-import HeaderComponent from './components/Header.vue'
-import FooterComponent from './components/FooterComponent'
-import LastArticles from './components/LastArticles'
-import Slider from './components/Slider.vue'
-import Sidebar from './components/Sidebar.vue'
+import HeaderComponent from "./components/Header.vue";
+import FooterComponent from "./components/FooterComponent";
+import LastArticles from "./components/LastArticles";
+import Slider from "./components/Slider.vue";
+import Sidebar from "./components/Sidebar.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HeaderComponent,
     FooterComponent,
@@ -26,17 +25,8 @@ export default {
     Slider,
     Sidebar
   }
-}
+};
 </script>
-
 <style>
-/*#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}*/
-@import './assets/style.css'
+@import "./assets/style.css";
 </style>
