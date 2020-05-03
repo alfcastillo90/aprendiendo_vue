10082728b6a8e807fee829effd2adae981569212
alfcastillo90 <cacr1990@gmail.com>
2020-05-03 19:09:12 -0400

    [ADD]: routing y navegacion

---
+++ src/components/Pagina.vue
@@ -0,0 +1,44 @@
+<template>
+    <section id="content">
+        <h2 class="subheader">Página</h2>
+        <h3>{{id_desde_url}}</h3>
+        <button @click="actualizarNombre()">Actualizar nombre</button>
+        <button @click="redirigirAlBlog()">Redirigir</button>
+        <h4>{{nombre}}</h4>
+    </section>
+</template>
+<script>
+export default {
+    name: "Pagina",
+    created(){ // cuando el componente se crea pero no se ha terminado de cargar
+        console.log('componente creado')
+    },
+    mounted() { // cuando el componente se inicia
+        this.id_desde_url = this.$route.params.id;
+        console.log('componente montado');
+    },
+    updated(){
+        console.log('componente actualizado');
+    },
+    destroyed() { // Se ejecuta cuando el componente se elimina
+        console.log('componente eliminado');
+    },
+    data(){
+        return {
+            id_desde_url: null,
+            nombre: 'Carlos Castillo'
+        }
+    },
+    methods: {
+        actualizarNombre() {
+            this.nombre = 'Carlos Alfredo Castillo Rodriguez';
+            console.log(this.nombre)
+        },
+        redirigirAlBlog() {
+            this.$router.push({name:'pagina', params: {id: 'ruta redirigida' }});
+        }
+    }
+    
+
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -4,15 +4,26 @@ import VueRouter from 'vue-router'
 //Componentes creados
 import LastArticles from './components/LastArticles.vue'
 import MiComponente from './components/MiComponente.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Blog from './components/Blog.vue'
+import Formulario from './components/Formulario.vue'
+import Pagina from './components/Pagina.vue'
+import NoEncontrado from './components/NoEncontrado.vue'
+
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
 const routes = [
-  {path: '/', component: LastArticles},
-  {path: '/home', component: LastArticles},
-  {path: '/mi-componente', component: MiComponente},
-  {path: '/ultimos-articulos', component: LastArticles}
+  { path: '/', component: LastArticles},
+  { path: '/home', component: LastArticles},
+  { path: '/blog', component: Blog},
+  { path: '/formulario', component: Formulario},
+  { path: '/pagina/:id?', name:'pagina', component: Pagina},
+  { path: '/ultimos-articulos', component: LastArticles},
+  { path: '/mi-componente', component: MiComponente},
+  { path: '/hola-mundo', component: HelloWorld},
+  { path: '*', component: NoEncontrado}
 ];
 
 const router = new VueRouter({
