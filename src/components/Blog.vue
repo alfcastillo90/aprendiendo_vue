ec32d3bff7156ede20220174e76a16bebeafacbb
alfcastillo90 <cacr1990@gmail.com>
2020-05-07 00:49:10 -0400

    [ADD]: http y peticiones asincronas

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -3,7 +3,10 @@
     <Slider texto="Blog"></Slider>
     <div class="center">
       <section id="content">
-        <h2 class="subheader">Blog</h2>
+        <h1 class="subheader">Blog</h1>
+        <div id="articles" v-if="articles">
+          <Articles :articles="articles"></Articles>
+        </div>
       </section>
     </div>
     <Sidebar></Sidebar>
@@ -11,14 +14,37 @@
   </div>
 </template>
 <script>
+import axios from 'axios';
+import Global from '../Global';
 import Slider from "./Slider.vue";
 import Sidebar from "./Sidebar.vue";
+import Articles from "./Articles.vue";
 
 export default {
   name: "Blog",
-   components: {
+  components: {
+    Articles,
     Slider,
     Sidebar
+  },
+  data(){
+    return {
+      url: Global.url,
+      articles: []
+    }
+  },
+  mounted(){
+    this.getArticles();
+  },
+  methods: {
+    getArticles(){
+      axios.get(this.url + 'articles')
+      .then(res => {
+        if (res.data.status == 'success') {
+          this.articles = res.data.articles;
+        }
+      })
+    }
   }
 };
 </script>
